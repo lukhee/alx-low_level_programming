3ae32b848e51d9a21dd80bff5793e469bed7094b
“Balogun <lukheebalo@gmail.com>
2022-07-01 17:19:34 -0700

    Print all alphabet  except e and q

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int alpha;
+for (alpha = 97; alpha <= 122; alpha++)
+{
+	putchar(alpha);
+}
+for (alpha = 65; alpha <= 90; alpha++)
+{
+	putchar(alpha);                                            
+}
+putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int alpha;
+for (alpha = 97; alpha <= 122; alpha++)
+{
+	if(alpha != 113 || alpha != 101 )
+	{
+		puitchar(alpha);
+	}
+}
+putchar(10);
+return (0);
+}

b2fd1637b10e2b09d6d56802ea2b948077cd55fc
“Balogun <lukheebalo@gmail.com>
2022-07-01 18:49:34 -0700

    number arrangemment

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int alpha, n, m;
+for (alpha = 48; alpha <= 57; alpha++)
+{
+	for (n = 49; n <= 57; n++)
+	{
+		for (m = 50; m <= 57; m++)
+		{
+			
+			if (n > alpha && m > n)
+			{
+				putchar(alpha);
+				putchar(n);
+				putchar(m);
+				if (alpha != 55 || n != 56 || m != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+}
+putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int alpha, n;
+for (alpha = 48; alpha <= 57; alpha++)
+{
+	for (n = 49; n <= 57; n++)
+	{
+		putchar(alpha);
+		putchar(alpha);
+		putchar(32);
+		putchar(alpha);
+		putchar(n);
+		if (alpha != 56 || n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+}
+putchar(10);
+return (0);
+}

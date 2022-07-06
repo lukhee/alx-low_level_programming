42673ab65b42385b34c906ea5bdcdd57bc70535a
“Balogun <lukheebalo@gmail.com>
2022-07-05 19:30:52 -0700

    Function in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include "main.h";
+#include "main.h"
 /**
  *main - Entry point
  *
- *Return: Always 0 (Success)
+ *Return: Always (Success)
  */
-int main(void)
+int main (void)
 {
 	_putchar('_');
 	_putchar('p');

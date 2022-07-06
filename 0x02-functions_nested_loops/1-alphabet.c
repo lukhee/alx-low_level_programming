728383203cac080e54f1d8cc01120693d232bc08
“Balogun <lukheebalo@gmail.com>
2022-07-05 19:52:08 -0700

    Print lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Main Page
+ *
+ * print alphabet in lower case
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	for(j = "a"; j <= "z"; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet_x10
+
+	return(0)
+}

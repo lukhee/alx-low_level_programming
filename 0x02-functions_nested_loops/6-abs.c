766a998ab6fa26148245717b9f93824212643293
“Balogun <lukheebalo@gmail.com>
2022-07-05 21:21:46 -0700

    Print last digit of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,6 +9,6 @@
 int _abs(int n)
 {
 	if (n < 0)
-		n = -n
-	return n
+		n = -n;
+	return (n)
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> check if the character is alphabet
+ * @n: a charaacter argument
+ * Return: return  if  lower and 0 is nnot
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n
+	x = n % 10;
+	return n
+}

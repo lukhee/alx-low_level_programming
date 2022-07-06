6104c16e3f8604b5393d458749339794c35a4135
“Balogun <lukheebalo@gmail.com>
2022-07-05 21:32:45 -0700

    Hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _abs -> check if the character is alphabet
+ * jack_bauer -> check if the character is alphabet
  * @n: a charaacter argument
  * Return: return  if  lower and 0 is nnot
  */
 
-int _abs(int n)
+void jack_bauer(void)
 {
-	if (n < 0)
-		n = -n;
-	return (n);
+	int x;
+
 }

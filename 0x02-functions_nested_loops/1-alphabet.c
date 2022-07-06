03c684314a7ff9d694560117f0c7272d601dfe92
“Balogun <lukheebalo@gmail.com>
2022-07-05 20:11:01 -0700

    Print

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * Main Page
+ * main - check the code
  *
- * print alphabet in lower case
+ * Return: Alwaays 0
  */
 
 void print_alphabet(void)
 {
 	char j;
+
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);

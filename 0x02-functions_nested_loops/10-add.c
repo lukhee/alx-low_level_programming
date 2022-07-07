7bc6feb9f70404680198416ab21f130aadc4419f
“Balogun <lukheebalo@gmail.com>
2022-07-06 18:44:02 -0700

    Add two input

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add -> add 2 input  together
+ * 
+ * @m -> first input
+ *
+ * @nn -> second input
+ *
+ * Return: return  if  lower and 0 is nnot
+ */
+
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+
+	return (result + 48);
+	
+}

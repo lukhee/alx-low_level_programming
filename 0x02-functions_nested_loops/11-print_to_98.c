856e4ba782ddb335d06863d532d44d2f781478ca
“Balogun <lukheebalo@gmail.com>
2022-07-06 19:00:48 -0700

    Print number till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 -> print n to 98
+ * @n: n number of time
+ * Return: return  if  lower and 0 is nnot
+ */
+
+void print_to_98(int n)
+{
+	int i;
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else 
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i)
+		}
+	}
+}

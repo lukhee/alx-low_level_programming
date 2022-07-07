9d455b5e625e61d3b687168367a6a497982d98ef
“Balogun <lukheebalo@gmail.com>
2022-07-06 17:07:51 -0700

    Hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,9 @@
 int print_last_digit(int n)
 {
 	int x;
+
 	x = n % 10;
+
 	if (x < 0)
 		x = -x;
 	_putchar(x + '0');
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,5 +9,24 @@
 void jack_bauer(void)
 {
 	int x;
+	int j;
+	int i;
 
+	for (x = 0; x < 24; x++)
+	{
+		for (i = 0; i < 60; i++)
+		{
+			for (j = 0; j  < 60; j++)
+			{
+				_putchar(x);
+				_putchar(i);
+				_putchar(':');
+				if (j < 10)
+				{
+					_putchar(0);
+				}
+				_putchar(j);
+			}
+		}
+	}
 }
